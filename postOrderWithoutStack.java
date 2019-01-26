b9e020c38b8015e96b142a0844f186979ff97f3b
MPR <raleraskar.mayur@gmail.com>
2019-01-25 20:22:21 -0800

    Update postOrderWithoutStack.java

--- postOrderWithoutStack.java
+++ postOrderWithoutStack.java
@@ -1,33 +1,22 @@
-
-
-
 postOrder(Node root){
+      if root = null return;
 
-while(true){
-  while(root != null){
-    stack.push(root)
-    root =  root.left
-  }
-
- while(stack.isNotEmpty){
-    if(stack.peek.right = null or printedNode = stack.peek.right){
-       print stack.peek
-       printedNode = stack.pop
-       
-       if(stack.isEmpty) return;
-       
-       if(printedNode = stack.peek.left && stack.peek.right != null){
-          stack.push(stack.peek.right)
-          break;
-       }
-    }
- }
-
-}   
-
-   
-  
-
-
+      stack.push(root)
+      moveLeftMost()
+      justPrinted = NULL
+      while(stack is not empty){
+          if justPrinted == stack.top.right{
+             leaf = stack.pop()
+             justPrinted = leaf
+             print leaf.data
+           }else justPrinted == stack.top.left {
+              if stack.top.right!=null{
+                root = stack.top.right
+                stack.push(root)
+                moveLeftMost()
+              }
+              justPrinted = null
+           }
+      }
 
 }
